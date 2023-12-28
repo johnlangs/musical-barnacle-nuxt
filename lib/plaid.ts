ed55e45f24c8f312b488cd71810d66ebcc5aa78e
John Langs <me@johnlangs.dev>
2023-12-28 03:10:58 -0600

    test

--- lib/plaid.ts
+++ lib/plaid.ts
@@ -1,4 +1,6 @@
 import { Configuration, PlaidApi, PlaidEnvironments } from "plaid";
+import fetchAdapter from "@haverstack/axios-fetch-adapter";
+
 const config = new Configuration({
     basePath: PlaidEnvironments[process.env.PLAID_ENV!],
     baseOptions: {
@@ -6,7 +8,8 @@ const config = new Configuration({
         "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
         "PLAID-SECRET": process.env.PLAID_SECRET,
         "Plaid-Version": "2020-09-14",
-      }
+      },
+      adapter: fetchAdapter
     },
 });
 

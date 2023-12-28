8ba8cbb53b9700fece38040f766e8e231ab30ec5
John Langs <me@johnlangs.dev>
2023-12-28 00:42:23 -0600

    Added header element

--- app.vue
+++ app.vue
@@ -1,5 +1,42 @@
+<script setup lang="js">
+import Header from './components/Header.vue';
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <Header></Header>
+    <div class="global">
+      
+    </div>
   </div>
 </template>
+
+<style>
+/*Inter Font*/
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap');
+
+/* Style sheet for MyApp.jsx */
+
+/* padding should be applyed around all cards, 15px*/
+.global {
+  padding:15px;
+}
+
+.cardTitle {
+  font-size: 1.5em;
+  font-weight: 400;
+  margin: inherit;
+}
+
+html,body { height: 100%; margin: 0px}
+body {
+  font-family: 'Inter', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,105 @@
+<script setup lang="js">
+import { ref } from 'vue'
+
+const balance = ref(0)
+const accounts = ref(null)
+const loading = ref(true)
+
+const fetchData = async () => {
+    // loading.value = true
+
+    // const response = await useFetch()
+    // const data = await response.json()
+
+    // accounts.value = data.accounts.map((item) => item.name).join(", ")
+    // balance.value = "$ " + data.balance.toLocaleString()
+
+    // loading.value = false
+}
+
+onMounted(() => (
+    fetchData()
+))
+
+</script>
+
+<template>
+    <section class="HeaderContainer">
+        <!-- Balance Amount Field -->
+        <p v-if="loading" id="accountSum"> $ Loading . . . </p>
+        <p v-else id="accountSum"> {{ balance }} </p>
+
+        <!-- Bank List Field -->
+        <p v-if="loading" id="bankList">
+            <b> Accounts Connected: </b>
+            Loading . . .
+        </p>
+        <p v-else id="bankList">
+            <b> Accounts Connected: </b>
+            {{ accounts }}
+        </p>
+    </section>
+</template>
+
+<style scoped>
+/* CSS needed for Header.jsx */
+
+/* Used to contain header, provides background color*/
+.HeaderContainer {
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-bottom: 60px;
+  background-color: #F2A1FF;
+  font-family: Inter;
+}
+
+/* large number*/
+#accountSum {
+  margin:inherit;
+  font-size: 2.8125rem;
+  font-size: clamp(2.7rem, 1.6rem + 5.4vw, 6rem);
+  font-weight:900;
+}
+@media screen and (min-width: 1000px) {
+  #accountSum {
+    font-size: 96px;
+  }
+}
+
+/* list of banks the user has connected */
+#bankList {
+  margin:inherit;
+  font-size:calc(10px + .45vw); /*min width of 10px*/
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+@media screen and (min-width: 1000px) {
+  #bankList {
+    font-size: 15px
+  }
+}
+
+.dialogButton {
+  margin:inherit;
+}
+
+.dialogButton p {
+  font-size: 1.25rem;
+  margin: calc(5px + .25vw);
+}
+
+/*Card that shows the user their options*/
+#cardSelector {
+  margin-top: calc(10px + .5vw);
+  pad: 10px;
+  text-align: center;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 400;
+  border-radius: 10px;
+}
+#cardSelector p {
+  color:  black;
+}
+</style>
